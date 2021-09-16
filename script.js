73db663869e0152b59d757d7b18cf745a6ff688a
Madhavi Latha Yayavaram <87151264+MadhaviLathaYayavaram@users.noreply.github.com>
2021-09-16 13:26:42 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,42 @@
+var txtNum = document.createElement('h1');
+txtNum.setAttribute("align","center");
+txtNum.style.color="crimson";
+txtNum.style.fontSize="100px";
+txtNum.style.verticalAlign='center';
+document.body.append(txtNum);
+
+setTimeout(() => {
+       txtNum.innerHTML = "10";
+ 
+  setTimeout(() => {
+        txtNum.innerHTML = "9";
+  setTimeout(() => {
+        txtNum.innerHTML = "8";
+   setTimeout(() => {
+        txtNum.innerHTML = "7";
+     
+   setTimeout(() => {
+        txtNum.innerHTML = "6";
+   setTimeout(() => {
+        txtNum.innerHTML = "5";  
+   setTimeout(() => {
+        txtNum.innerHTML = "4";  
+    setTimeout(() => {
+        txtNum.innerHTML = "3"; 
+    setTimeout(() => {
+        txtNum.innerHTML = "2";  
+    setTimeout(() => {
+        txtNum.innerHTML = "1";  
+     setTimeout(() => {  
+      txtNum.innerHTML ="Happy New Year!";
+         },2000);
+         },2000);
+        },2000);
+       },2000);
+       },2000);   
+      },2000); 
+     },2000); 
+    },2000); 
+   },2000); 
+  },2000); 
+},2000);
